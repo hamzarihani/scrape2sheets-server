0ec423a0657723d433fc95c0a98feeb769ea2320
Hamza Rihani <hamzarihani66@gmail.com>
2026-02-23 17:46:25 +0100

    Update instrument.js

--- backend/instrument.js
+++ backend/instrument.js
@@ -3,10 +3,10 @@ const Sentry = require('@sentry/node');
 if (process.env.SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
-    environment: process.env.NODE_ENV || 'development',
+    environment: process.env.NODE_ENV || 'production',
     tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
     // Setting this option to true will send default PII data to Sentry
-    sendDefaultPii: false,
+    sendDefaultPii: true,
   });
 }
 
