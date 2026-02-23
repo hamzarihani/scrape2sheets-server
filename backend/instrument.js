13f65251ee66d05cfb11f3dfa037b6dd0e035740
Hamza Rihani <hamzarihani66@gmail.com>
2026-02-23 17:50:41 +0100

    Update instrument.js

--- backend/instrument.js
+++ backend/instrument.js
@@ -5,7 +5,7 @@ if (process.env.SENTRY_DSN) {
     dsn: process.env.SENTRY_DSN,
     environment: process.env.NODE_ENV || 'production',
     tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.1 : 1.0,
-    // Setting this option to true will send default PII data to Sentry
+    /// Setting this option to true will send default PII data to Sentry
     sendDefaultPii: true,
   });
 }
